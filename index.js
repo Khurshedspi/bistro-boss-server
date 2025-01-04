94568d10adb83d8ca38086560acb00bfd946caec
Khurshedspi <khurshed5322@gmail.com>
2025-01-05 02:41:05 +0600

    create: menu and reviews db and collection and also get menu and reviews data by using find method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require ('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config( );
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -9,9 +9,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.fmklx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fmklx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,29 +18,49 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    // start code here
+
+    //create database and collection for menu data in mongodb  with using db method and collection method
+    const menuCollection = client.db("bistroDb").collection("menu");
+    //create database and collection for reviews data in mongodb  with using db method and collection method
+
+    const reviewsCollection = client.db("bistroDb").collection("reviews");
+
+    // for get menu's  all data from mongodb with using find method
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+    // for get menu's  all data from mongodb with using find method
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("boss is sitting");
+});
 
-app.get('/', (req, res) =>{
-    res.send('boss is sitting')
-})
-
-
-app.listen(port, () =>{
-    console.log(`Bistro Boss is sitting on port $(port)`);
-})
+app.listen(port, () => {
+  console.log(`Bistro Boss is sitting on port $(port)`);
+});
