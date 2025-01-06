9e8884a8976a9d41a11d491e4b602e9a18c39494
Khurshedspi <khurshed5322@gmail.com>
2025-01-07 02:16:33 +0600

    add: find single cart by using get method and also insert one cart by using post and also added delete by using deleteOne method

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fmklx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,15 +33,39 @@ async function run() {
 
     const reviewsCollection = client.db("bistroDb").collection("reviews");
 
+    const cartsCollection = client.db("bistroDb").collection("carts");
+
     // for get menu's  all data from mongodb with using find method
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
 
-    // for get menu's  all data from mongodb with using find method
+    // for get reviews's  all data from mongodb with using find method
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // for insert cart's data in mongodb with using insertOne method
+
+    // carts collection
+
+    app.get("/carts", async (req, res) => {
+      const result = await cartsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartsCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -64,3 +88,17 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Bistro Boss is sitting on port $(port)`);
 });
+
+/**
+ * ------------------------
+ * NAMING CONVENTION
+ * ---------------------------
+ *
+ * 1. app.get('/users), for get all users data
+ * 2. app.get('/users/:id), for get single user data
+ * 3. app.post('/users'), for insert user data
+ * 4. app.put('/users/:id'), for update user data
+ * 5. app.patch('/users/:id'), for update user data
+ * 6. app.delete('/users/:id'), for delete user data
+ * 7.
+ */
